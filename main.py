a157fcd949ed2b601299c0b3f9357e1dd1df61ea
Humaira Muzammil <humaihr469@gmail.com>
2024-07-09 07:34:14 +0530

    update complete version

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import random
 
 cardNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-
 userNumbers = []
 computerNumbers = []
 
@@ -9,9 +8,15 @@ def game_start():
     global userNumbers, computerNumbers
     userNumbers = []
     computerNumbers = []
+
     gameChoice = input("Do you want to play a game of Blackjack? Enter 'y' to start the game. Enter 'n' to exit : ").lower()
     print()
-    if gameChoice == "y":
+
+    if gameChoice == "n":
+        print("Sorry to see you go. Have a nice day!")
+        return False
+
+    elif gameChoice == "y":
         print(r''' 
         $$$$$$$\  $$\                     $$\                               $$\       
         $$  __$$\ $$ |                    $$ |                              $$ |      
@@ -27,8 +32,6 @@ def game_start():
           ''')
 
         user_cards1()
-
-        # create a function block by defining appropriate variables
         user_round()
 
         if userScore >= 21:
@@ -37,8 +40,6 @@ def game_start():
         else:
             gameContinuation = input("Type 'y' to get another card, type 'n' to pass : ").lower()
             print()
-
-            # roundNumber_user += 1
 
             while gameContinuation == "y":
                 user_cardsx()
@@ -51,11 +52,21 @@ def game_start():
                     print()
 
             if gameContinuation == "n":
+                computer_cardsx()
                 exceededScore()
 
-    else:
-        print("Sorry to see you go, have a nice day!")
+            else:
+                print("Invalid Entry!")
 
+    else:
+        print("Invalid Entry!")
+
+    return True
+
+def computer_cardsx():
+    while sum(computerNumbers) < 19:
+        computerNumbers.append(random.choice(cardNumbers))
+    scores()
 
 def ace_value(numbers):
     for i in range(len(numbers)):
@@ -63,8 +74,6 @@ def ace_value(numbers):
             numbers[i] = 11
     return numbers
 
-
-# noinspection PyTypeChecker
 def user_cards1():
     for x in range(2):
         userNumbers.append(random.choice(cardNumbers))
@@ -77,19 +86,16 @@ def user_cardsx():
     userNumbers.append(random.choice(cardNumbers))
     scores()
 
-
 def scores():
     global userScore, computerScore
     ace_value(userNumbers)
     userScore = sum(userNumbers)
-    ace_value(userNumbers)
+    ace_value(computerNumbers)
     computerScore = sum(computerNumbers)
-
 
 def user_round():
     print(f"\tYour cards: {userNumbers}, current score: {userScore}")
     print(f"\tComputer's first card: [{computerNumbers[0]}]\n")
-
 
 def exceededScore():
     print(f"\tYour final hand: {userNumbers}, final score: {userScore}")
@@ -113,33 +119,5 @@ def exceededScore():
             print("The Computer wins!")
 
 while True:
-    game_start()
-
-# def play_blackjack ():
-#
-#     if userScore >= 21:
-#         exceededScore()
-#
-#     else:
-#         gameContinuation = input("Type 'y' to get another card, type 'n' to pass : ").lower()
-#         print()
-#
-#         # roundNumber_user += 1
-#
-#         while gameContinuation == "y":
-#             user_cardsx()
-#             user_round()
-#             if userScore >= 21:
-#                 exceededScore()
-#                 break
-#             else:
-#                 gameContinuation = input("Type 'y' to get another card, type 'n' to pass : ").lower()
-#                 print()
-#
-#         if gameContinuation == "n":
-#             exceededScore()
-#
-# play_blackjack()
-#
-#
-#
+   if not game_start():
+       break
