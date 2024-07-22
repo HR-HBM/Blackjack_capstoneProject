9ae89165f1d533c1222ec31718865e792ebdef61
Humaira Muzammil <humaihr469@gmail.com>
2024-07-22 10:58:35 +0530

    revised version

--- main.py
+++ main.py
@@ -1,93 +1,50 @@
 import random
 from image import logo
 cardNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-userNumbers = []
-computerNumbers = []
+userCards = []
+computerCards = []
 
-def game_start():
-    global userNumbers, computerNumbers
-    userNumbers = []
-    computerNumbers = []
 
-    gameChoice = input("Do you want to play a ga`me of Blackjack? Enter 'y' to start the game. Enter 'n' to exit : ").lower()
-    print()
 
-    if gameChoice == "n":
-        print("Sorry to see you go. Have a nice day!")
-        return False
+def add_card_to_computer():
+    while sum(computerCards) < 17:
+        computerCards.append(random.choice(cardNumbers))
+    calculate_score()
 
-    elif gameChoice == "y":
-        print(logo)
-
-        user_cards1()
-        user_round()
-
-        if userScore >= 21:
-            exceededScore()
-
-        else:
-            gameContinuation = input("Type 'y' to get another card, type 'n' to pass : ").lower()
-            print()
-
-            while gameContinuation == "y":
-                user_cardsx()
-                user_round()
-                if userScore >= 21:
-                    exceededScore()
-                    break
-                else:
-                    gameContinuation = input("Type 'y' to get another card, type 'n' to pass : ").lower()
-                    print()
-
-            if gameContinuation == "n":
-                computer_cardsx()
-                exceededScore()
-
-            else:
-                print("Invalid Entry!")
-
-    else:
-        print("Invalid Entry!")
-
-    return True
-
-def computer_cardsx():
-    while sum(computerNumbers) < 19:
-        computerNumbers.append(random.choice(cardNumbers))
-    scores()
-
-def ace_value(numbers):
+def set_ace_value(numbers):
     for i in range(len(numbers)):
-        if numbers[i] == cardNumbers[0] and sum(numbers) <= 11:
+        if numbers[i] == 1 and sum(numbers) <= 11:
             numbers[i] = 11
-    return numbers
+    if all(number == 1 for number in numbers[:2]):
+        numbers[0] = 11
+        numbers[1] = 1
 
-def user_cards1():
+def serve_cards():
     for x in range(2):
-        userNumbers.append(random.choice(cardNumbers))
+        userCards.append(random.choice(cardNumbers))
     for y in range(2):
-        computerNumbers.append(random.choice(cardNumbers))
+        computerCards.append(random.choice(cardNumbers))
 
-    scores()
+    calculate_score()
 
-def user_cardsx():
-    userNumbers.append(random.choice(cardNumbers))
-    scores()
+def add_card_to_user():
+    userCards.append(random.choice(cardNumbers))
+    calculate_score()
 
-def scores():
+def calculate_score():
     global userScore, computerScore
-    ace_value(userNumbers)
-    userScore = sum(userNumbers)
-    ace_value(computerNumbers)
-    computerScore = sum(computerNumbers)
+    set_ace_value(userCards)
+    userScore = sum(userCards)
+    set_ace_value(computerCards)
+    computerScore = sum(computerCards)
 
-def user_round():
-    print(f"\tYour cards: {userNumbers}, current score: {userScore}")
-    print(f"\tComputer's first card: [{computerNumbers[0]}]\n")
+def print_scores():
+    print(f"\tYour cards: {userCards}, current score: {userScore}")
+    print(f"\tComputer's first card: [{computerCards[0]}]\n")
 
-def exceededScore():
-    print(f"\tYour final hand: {userNumbers}, final score: {userScore}")
-    print(f" \tComputer's final hand: {computerNumbers}, final score: {computerScore}\n\n")
+def finalize_scores():
+    print(f"\tYour final hand: {userCards}, final score: {userScore}")
+    print(f" \tComputer's final hand: {computerCards}, final score: {computerScore}\n\n")
     if userScore == computerScore:
         print("It's a Draw!")
     elif computerScore < userScore > 21:
@@ -106,6 +63,53 @@ def exceededScore():
         elif computerScore > userScore:
             print("The Computer wins!")
 
+def start_game():
+    global userCards, computerCards
+    userCards = []
+    computerCards = []
+
+    userChoice = input("Do you want to play a game of Blackjack? Enter 'y' to start the game. Enter 'n' to exit : ").lower()
+    print()
+
+    if userChoice == "n":
+        print("Sorry to see you go. Have a nice day!")
+        return False
+
+    elif userChoice == "y":
+        print(logo)
+
+        serve_cards()
+        print_scores()
+
+        if userScore >= 21:
+            finalize_scores()
+
+        else:
+            while True:
+                gameContinuation = input("Type 'y' to get another card, type 'n' to pass : ").lower()
+                print()
+
+                if gameContinuation == "y":
+                    add_card_to_user()
+                    print_scores()
+                    if userScore >= 21:
+                        finalize_scores()
+                        break
+
+                elif gameContinuation == "n":
+                    add_card_to_computer()
+                    finalize_scores()
+                    break
+                else:
+                    print("Invalid Entry!")
+
+    else:
+        print("Invalid Entry!")
+
+    return True
+
+
+
 while True:
-   if not game_start():
+   if not start_game():
        break
